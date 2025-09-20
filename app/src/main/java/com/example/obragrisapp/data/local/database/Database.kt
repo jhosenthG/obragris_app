1e7e6c00f063c38ef86b60874507bc6c8a11c3db
jhosenthg <jhosenth@gmail.com>
2025-09-20 11:43:33 -0400

    add database for project

---
+++ app/src/main/java/com/example/obragrisapp/data/local/database/Database.kt
@@ -0,0 +1,23 @@
+package com.example.obragrisapp.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.obragrisapp.data.local.dao.ProjectDao
+import com.example.obragrisapp.data.local.dao.UserDao
+import com.example.obragrisapp.data.local.entities.Project
+import com.example.obragrisapp.data.local.entities.User
+
+@Database(
+    entities = [
+        Project::class,
+        User::class
+               ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(TypeConverters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun projectdao(): ProjectDao
+    abstract fun userdao(): UserDao
+}
