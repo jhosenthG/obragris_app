5b934c34db958be9d12d3bee4e955af252817da4
JhosenthG <jhosenth@gmail.com>
2025-09-27 18:14:35 -0400

    add di for main module

--- app/src/main/java/com/example/obragrisapp/MainActivity.kt
+++ app/src/main/java/com/example/obragrisapp/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.obragrisapp.presentation.theme.ObragrisAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
