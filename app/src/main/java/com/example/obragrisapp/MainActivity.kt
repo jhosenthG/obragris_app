ad432ffbcfb17a79c47c0566734731a6b76c82eb
jhosenthG <jhosenth@gmail.com>
2025-10-01 18:51:57 -0400

    add NavStack for main activity

--- app/src/main/java/com/example/obragrisapp/MainActivity.kt
+++ app/src/main/java/com/example/obragrisapp/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.obragrisapp.presentation.navigation.navStack
 import com.example.obragrisapp.presentation.theme.ObragrisAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,29 +23,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ObragrisAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    navStack(navControler = rememberNavController())
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ObragrisAppTheme {
-        Greeting("Android")
-    }
-}
