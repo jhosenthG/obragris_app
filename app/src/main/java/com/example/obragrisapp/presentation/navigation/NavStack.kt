484bdfe42a59edd8bbd83ccc4167a219b6c89e6b
jhosenthG <jhosenth@gmail.com>
2025-10-01 18:51:40 -0400

    add navhost for navigation

--- app/src/main/java/com/example/obragrisapp/presentation/navigation/NavStack.kt
+++ app/src/main/java/com/example/obragrisapp/presentation/navigation/NavStack.kt
@@ -1,3 +1,35 @@
 package com.example.obragrisapp.presentation.navigation
 
-sealed class NavStack()
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.obragrisapp.presentation.screen.home.HomeScreen
+import com.example.obragrisapp.presentation.screen.login.LoginScreen
+
+@Composable
+fun navStack(navControler: NavHostController) {
+    NavHost(navControler, startDestination = NavRoutes.Login.routes) {
+        composable(NavRoutes.Login.routes) {
+            LoginScreen(
+                onLoginSuccess = {
+                    navControler.navigate(NavRoutes.Login.routes) {
+                        popUpTo(NavRoutes.Login.routes) { inclusive = true }
+                    }
+                    composable(NavRoutes.Home.routes) {
+                        HomeScreen(navControler)
+                    }
+                    composable(NavRoutes.Project.routes) {
+
+                    }
+                    composable(NavRoutes.Report.routes) {
+
+                    }
+                    composable(NavRoutes.User.routes) {
+
+                    }
+                }
+            )
+        }
+    }
+}
