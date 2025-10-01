2b23a6cd8be00e8a9bcd0be19919ca02c8ec2733
jhosenthG <jhosenth@gmail.com>
2025-10-01 18:52:33 -0400

    add color for components

--- app/src/main/java/com/example/obragrisapp/presentation/theme/Color.kt
+++ app/src/main/java/com/example/obragrisapp/presentation/theme/Color.kt
@@ -9,3 +9,25 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Primary color for component
+val primaryLightContrast = Color(0xFF022739)
+val onPrimaryLightContrast = Color(0xFFFFFFFF)
+
+//Secondary color for component
+val secondaryLightContrast = Color(0xFF548351)
+val onSecondaryLightContrast = Color(0xFFFFFFFF)
+
+//Background color for component
+val backgroundLightContrast = Color(0xFFF1F1F1)
+val onBackgroundLightContrast = Color(0xFF181C20)
+
+//Surface color for component
+val SurfaceLightContrast = Color(0xFFF5FAFC)
+
+//Buttons color for component
+
+
+// Error color for component
+val ErrorLightContrast = Color(0xFFBB1C1C)
+val onErrorLightContrast = Color(0xFFFFFFFF)
