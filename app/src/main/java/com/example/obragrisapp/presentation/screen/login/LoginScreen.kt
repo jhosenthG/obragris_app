22bc28e4bfc85009275f6ef15a245eeeba1938ae
jhosenthG <jhosenth@gmail.com>
2025-10-01 18:52:25 -0400

    add temporary screen fix

--- app/src/main/java/com/example/obragrisapp/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/obragrisapp/presentation/screen/login/LoginScreen.kt
@@ -1,17 +1,46 @@
 package com.example.obragrisapp.presentation.screen.login
 
 import android.R.attr.onClick
+import android.util.Log
 import android.widget.Button
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.obragrisapp.presentation.theme.ObragrisAppTheme
 
 @Composable
 fun LoginScreen(
-    navController: NavController,
+    onLoginSuccess: () -> Unit
 ) {
-    Text("Login")
-    Button(onClick = {}) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = onLoginSuccess,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Iniciar sesión")
+        }
     }
+}
+
+
+@Preview
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen {  }
 }
